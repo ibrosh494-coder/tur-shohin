c057624368052aac5efca757a568c2e83ba63c43
Tur Shohin <dev@turshohin.tj>
2026-09-10 14:38:51 +0500

    Главная: убраны «Популярные направления», «Лучшие туры», «Истории путешественников»

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,8 @@
 import { Seo } from '../lib/seo'
 import { Hero } from '../components/home/Hero'
-import { CategoriesSection, DestinationsSection } from '../components/home/Categories'
-import { ToursSection, WhyUs, Experience, OffersSection, HowItWorks } from '../components/home/Sections'
-import { ReviewsCarousel, GalleryPreview, StoriesSection, NewsletterSection } from '../components/home/Social'
+import { CategoriesSection } from '../components/home/Categories'
+import { WhyUs, Experience, OffersSection, HowItWorks } from '../components/home/Sections'
+import { ReviewsCarousel, GalleryPreview, NewsletterSection } from '../components/home/Social'
 
 export default function Home() {
   return (
@@ -10,13 +10,10 @@ export default function Home() {
       <Seo title="Тур Шохин — открой Таджикистан по-новому" description="Премиальные туры по Таджикистану и миру: Памирский тракт, Фанские горы, Искандеркуль. Онлайн-бронирование за минуту." />
       <Hero />
       <CategoriesSection />
-      <DestinationsSection />
-      <ToursSection />
       <WhyUs />
       <Experience />
       <OffersSection />
       <HowItWorks />
-      <StoriesSection />
       <ReviewsCarousel />
       <GalleryPreview />
       <NewsletterSection />
